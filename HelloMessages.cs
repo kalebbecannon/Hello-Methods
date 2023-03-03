9eae0005d966170941c04a725ba054bef628733f
kalebbecannon <kalebbecannon@gmail.com>
2023-03-02 19:18:24 -0600

    //Added h1 tag

---
+++ HelloMessages.cs
@@ -0,0 +1,24 @@
+namespace HelloMethods
+{
+    public class Message
+    {
+    public static string GetMessage(string lang)
+    {
+        if (lang.Equals("sp")) {
+            return "Hola Mundo";
+        }
+        else if (lang.Equals("fr"))
+        {
+            return "Bonjour le monde";
+        }
+        else if (lang.Equals("vm"))
+        {
+            return "Chào thế giới";
+        }
+        else
+        {
+            return "Hello World";
+        }
+        }
+    }
+}
